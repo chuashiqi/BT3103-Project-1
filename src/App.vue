3f9e15d40dc7e2f8d27e0ee466835ef88a1c57d6
chuashiqi <70755428+chuashiqi@users.noreply.github.com>
2021-03-08 22:58:29 +0800

    added name - shiqi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App Clement"/>
+    <HelloWorld msg="Welcome to Your Vue.js App Clement, Shi Qi"/>
   </div>
 </template>
 
