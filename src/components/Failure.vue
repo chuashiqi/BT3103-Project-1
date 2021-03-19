3849a31644f98384691e01c402050ffa647bc6f9
clairecky <78159773+clairecky@users.noreply.github.com>
2021-03-19 17:06:43 +0800

    Quiz Page ver 1.0
    Quiz Homepage
    Success Page
    Failure Page

---
+++ src/components/Failure.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="failure">
+    <img src="./assets/Failure.png">
+    <p>Better luck next time! <br> Click on the Information tab to learn more.</p>
+  </div>
+</template>
+
+<style>
+#failure {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 20px;
+  font-size: 45px;
+}
+</style>
---
+++ src/components/Success.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="success">
+    <img src="./assets/Success.png">
+    <p>You got it right! <br> 5 extra points have been awarded to your account.</p>
+  </div>
+</template>
+
+<style>
+#success {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 20px;
+  font-size: 45px;
+}
+</style>
