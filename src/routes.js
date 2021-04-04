d0ee36cf1c35f7b1ec5a619bd470a074b5f063e0
clementharsoyo <harsoyoclement@gmail.com>
2021-04-04 17:29:10 +0800

    FIxed routings -clem

--- src/routes.js
+++ src/routes.js
@@ -39,4 +39,6 @@ export default [
     {path:"/recyclingHistory", component: RecyclingHist},
     {path: "/benefitHistory2", component: BenefitHistory2},
     {path: "/profile", component: Profile},
+    {path: "/benefitHistory", component: BenefitHistory},
+    {path: "/forgotPassword", component: ForgotPassword}
 ]
