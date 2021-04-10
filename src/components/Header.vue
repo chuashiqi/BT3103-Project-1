cb2c58a6eb23783f4e5380db65610b33a9bb8d87
clairecky <78159773+clairecky@users.noreply.github.com>
2021-04-10 16:36:41 +0800

    Changing header to include dashboard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
         <router-link to = "/FAQ" exact>FAQ</router-link>
         <router-link to = "/rewardsPage" exact>Rewards</router-link>
         <router-link to = "/quizPage" exact>Quiz</router-link>
-        <router-link to = "/benefitHistory" exact>Benefit History</router-link>
+        <router-link to = "/dashboard" exact>Dashboard</router-link>
         <div class="header-right">
             <router-link to = "/profile" exact>My Account</router-link>
             <router-link @click.native="remove" to = "/" exact>Logout</router-link>
